4e1a483491a7f688de9bcf05eb6af40dc9e0d17d
EthanHNguyen <ethanhuunguyen@gmail.com>
2021-07-08 15:59:35 -0500

    Added multiple strip support

--- Arduino/SolidWhite/SolidWhite.ino
+++ Arduino/SolidWhite/SolidWhite.ino
@@ -1,46 +1,45 @@
 #include <FastLED.h>
 
 // How many leds in your strip?
-#define NUM_LEDS 300
-#define DATA_PIN 2
-#define BRIGHTNESS          1
-#define FRAMES_PER_SECOND  120
-#define MIDDLE_LED 111
-const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
-unsigned int sample;
-bool beat = false;
-CRGB leds[NUM_LEDS];
-int sensorPin = A5;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
-int ledOn = 0 ;
-unsigned int globL = 0;
-unsigned int globR = 0;
-int ti = 0;
-uint32_t timebase = 0;
-uint32_t phase = 49151;
-bool finished = true;
-bool running = false;
-int blueToothVal = 0;           //value sent over via bluetooth
-int Mode = 2;    //stores mode
-int Col = 6;    //stores Color
-int trueCol;
-
-typedef void (*SimplePatternList[])();
-
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
-uint8_t gHue = 100; // rotating "base color" used by many of the patterns
-
+#define NUM_LEDS_1 600
+#define NUM_LEDS_2 600
+#define NUM_LEDS_3 600
+#define DATA_PIN_1 5
+#define DATA_PIN_2 6
+#define DATA_PIN_3 7
+#define BRIGHTNESS 100
+CRGB leds_1[NUM_LEDS_1];
+CRGB leds_2[NUM_LEDS_2];
+CRGB leds_3[NUM_LEDS_3];
+int ledOn = NUM_LEDS_1;
 
 void setup() {
   Serial.begin(9600); // open the serial port at 9600 bps:
-  FastLED.addLeds<WS2812B, DATA_PIN>(leds, 300);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN_1>(leds_1, NUM_LEDS_1);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN_2>(leds_2, NUM_LEDS_2);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN_3>(leds_3, NUM_LEDS_3);
   FastLED.setBrightness(BRIGHTNESS);
-
-  for (int i = 0; i < NUM_LEDS; ++i) {
-    leds[i] = CRGB::White;
-  }
 }
 
 void loop() {
+  allWhite();
+}
+
+void allWhite() {
+  // LED Strip #1
+  for (int i = 0; i < NUM_LEDS_1; ++i) {
+    leds_1[i] = CRGB::White;
+  }
+
+  // LED Strip #2
+  for (int i = 0; i < NUM_LEDS_2; ++i) {
+    leds_2[i] = CRGB::White;
+  }
+
+  // LED Strip #3
+  for (int i = 0; i < NUM_LEDS_3; ++i) {
+    leds_3[i] = CRGB::White;
+  }
+
+  FastLED.show();
 }
